8d9cc77f89802a12efdf00e2594571c1c4ba8b4a
mariegoltermann <mari09a7@stud.kea.dk>
2022-08-28 11:14:21 +0200

    version2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
+
